483d34da8a8c1bc66cb3c1218c63609b5e91ef61
bosdo <bosdo@jfmedier.dk>
2020-03-22 22:06:04 +0100

    WIP - mongodb in parser

--- src/parser.ts
+++ src/parser.ts
@@ -1,6 +1,9 @@
 import { default as cheerio } from "cheerio";
-import * as fs from "fs";
+import { getFiles } from "./utils";
 import * as path from "path";
+import * as fs from "fs";
+import { default as moment } from "moment";
+import { default as mongodb } from "mongodb";
 
 //// Selectors
 const wrapperCellSel = ".lowfare-calendar__cell";
@@ -8,16 +11,28 @@ const priceSel = ".lowfare-calendar__price";
 const daySel = ".lowfare-calendar__date";
 
 const dumpPath = "dumps";
+const fileExt = ".txt";
 
 (async () => {
-  // TODO: read from buckets,
-  // mark processed files as done (rename/move)
-  const html = fs.readFileSync(`${dumpPath}${path.sep}lols.html`);
-  const data = [];
-  if (html) {
+  // TODO: switch out local file system with a cloud bucket
+  // TODO: mark processed files as done (rename/move)
+
+  // TODO: insert to DB!
+  //const dbUrl = 'mongodb://localhost:27017';
+  //const client = mongodb.connect(dbUrl)
+
+  const files = getFiles(dumpPath, fileExt);
+  console.log("files", files);
+  files.forEach(file => {
+    const fileBaseName = path.basename(file, fileExt);
+    console.log("fileBaseName", fileBaseName);
+    const parseDate = moment(fileBaseName, "YYYY-MM-DD-HH-mm-ss-SSS");
+    console.log("parseDate", parseDate);
+
+    const html = fs.readFileSync(file);
     const dom = cheerio.load(html);
 
-    dom(wrapperCellSel).map((i, el) => {
+    dom(wrapperCellSel).each((i, el) => {
       const day = parseInt(
         dom(el)
           .find(daySel)
@@ -30,5 +45,12 @@ const dumpPath = "dumps";
       );
       console.log("day => price", day, price);
     });
+  });
+  /*
+  const html = fs.readFileSync(`${dumpPath}${path.sep}**${path.sep}*.txt`);
+  const data = [];
+  if (html) {
+    
   }
+  */
 })();
