0ebb03b8c55b872b9d62f3f9206f3ed2af85c6ca
bosdo <bosdo@jfmedier.dk>
2020-03-25 08:23:43 +0100

    only rows with prices, grouped by origin

--- src/parser.ts
+++ src/parser.ts
@@ -4,71 +4,92 @@ import * as path from "path";
 import * as fs from "fs";
 import { default as moment } from "moment";
 
-//// Selectors
-const calendarSel = ".lowfare-calendar";
-const originSel = "#nas-airport-select-dropdown-input-0";
-const destinationSel = "#nas-airport-select-dropdown-input-1";
-const wrapperCellSel = ".lowfare-calendar__cell";
-const priceSel = ".lowfare-calendar__price";
-const daySel = ".lowfare-calendar__date";
-
-const dumpPath = "dumps";
-const fileExt = ".txt";
+interface ParsedFlightData {
+  flightDate: string;
+  price: number;
+  origin: string;
+  currency: string;
+}
+interface Entry extends ParsedFlightData {
+  createdAt: string;
+}
 
 export const parseAndStore = async (
   html: string | Buffer,
   datePeriod: string
-): Promise<Boolean> => {
+): Promise<any> => {
   if (!html) {
     return false;
   }
-  const d = parse(html, datePeriod);
 
-  const p = {
-    _id: "1",
-    scraped: moment().toISOString(),
-    // unique compound index
-    flightDate:"2",
-    price:2,
-    origin: "CPH",
-    currency: "DKK"
-  };
-  return true;
+  const createdAt = moment().toISOString();
+  const data: Entry[] = parse(html, datePeriod).map(row => {
+    return {
+      // unique compound index
+      ...row,
+      createdAt
+    };
+  });
+
+  return storeToDb(data);
 };
 
 const parse = (html: string | Buffer, datePeriod: string) => {
   const dom = cheerio.load(html);
-  const data = [];
-  let row;
+  const data: ParsedFlightData[] = [];
 
-  // TODO: separate between inbound and outbound flights
+  //// Selectors
+  const calendarSel = ".lowfare-calendar";
+  const wrapperCellSel = ".lowfare-calendar__cell";
+  const priceSel = ".lowfare-calendar__price";
+  const daySel = ".lowfare-calendar__date";
+
   // each calendar
-  dom(calendarSel).each((i, cal) => {
+  dom(calendarSel).each((calIdx, cal) => {
     // each cell
     dom(cal)
       .find(wrapperCellSel)
       .each((i, cell) => {
         dom(cell).each((i, cellContent) => {
-          const day = parseInt(
-            dom(cellContent)
-              .find(daySel)
-              .html()
-              ?.replace(".", "") || ""
-          );
           const price = parseInt(
             dom(cellContent)
               .find(priceSel)
               .html()
-              ?.replace(".", "") || ""
+              ?.replace(".", "") || "",
+            10
           );
-          console.log("day => price", day, price);
+
+          const day = parseInt(
+            dom(cellContent)
+              .find(daySel)
+              .html()
+              ?.replace(".", "") || "",
+            10
+          );
+
+          const flightDate = moment(
+            [datePeriod, day].join("-"),
+            "YYYY-MM-DD"
+          ).toISOString();
+
+          // only prices plz
+          if (!isNaN(price)) {
+            data.push({
+              flightDate,
+              price,
+              origin: calIdx === 0 ? "CPH" : "ALC",
+              currency: "DKK"
+            });
+          }
         });
       });
   });
+
+  return data;
 };
 
 // NoSQL
-const storeToDb = (data: any) => {};
+const storeToDb = (rows: Entry[]) => {};
 
 (async () => {
   // TODO: switch out local file system with a cloud bucket
@@ -78,20 +99,21 @@ const storeToDb = (data: any) => {};
   //const dbUrl = 'mongodb://localhost:27017';
   //const client = mongodb.connect(dbUrl)
 
-  const files = getFiles(dumpPath, fileExt);
+  const files = getFiles("dumps", ".txt");
   console.log("files", files);
   files.forEach(file => {
-    const fileBaseName = path.basename(file, fileExt);
+    const fileBaseName = path.basename(file, ".txt");
     console.log("fileBaseName", fileBaseName);
     const parseDate = moment(fileBaseName, "YYYY-MM-DD-HH-mm-ss-SSS");
     console.log("parseDate", parseDate);
 
     const html = fs.readFileSync(file);
 
-    const data = parse(html, "31");
+    const data = parse(html, "2020-03");
 
-    console.log("day => price", data);
+    console.log("data", data);
   });
+
   /*
   const html = fs.readFileSync(`${dumpPath}${path.sep}**${path.sep}*.txt`);
   const data = [];
